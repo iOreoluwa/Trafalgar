2cc10d03e6b29dc5d11441142374f0029a33b944
Adegboye Isaac <isaac.adegboye@yahoo.com>
2022-02-27 15:03:31 +0100

    designs

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,7 +1,7 @@
 export {default as Header} from './header/Header';
-export {default as Footer} from './footer/Footer';
-export {default as Testimonial} from './testimonial/Testimonial';
-export {default as Blog} from './blog/Blog';
+// export {default as Footer} from './footer/Footer';
+// export {default as Testimonial} from './testimonial/Testimonial';
+// export {default as Blog} from './blog/Blog';
 export {default as Service} from './service/Service';
-export {default as Provider} from './provider/Provider';
-export {default as Download} from './download/Download';
+// export {default as Provider} from './provider/Provider';
+// export {default as Download} from './download/Download';
