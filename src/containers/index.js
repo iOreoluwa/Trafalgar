470ec5e2309237f1936a5407f0db38832143e2a4
Adegboye Isaac <isaac.adegboye@yahoo.com>
2022-05-05 22:00:41 +0100

    fixes

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,5 +3,5 @@ export {default as Header} from './header/Header';
 // export {default as Testimonial} from './testimonial/Testimonial';
 // export {default as Blog} from './blog/Blog';
 export {default as Service} from './service/Service';
-export {default as Provider} from './provider/Provider';
+// export {default as Provider} from './provider/Provider';
 // export {default as Download} from './download/Download';
