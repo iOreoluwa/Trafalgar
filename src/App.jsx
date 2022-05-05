20cf086f792467a9e16fcfa8bf83b66b2de0821b
Adegboye Isaac <isaac.adegboye@yahoo.com>
2022-05-05 22:05:51 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import {Header, Footer, Blog, Download, Provider, Service, Testimonial, } from './containers';
-import {Header, Service} from './containers';
+import {Header, Service, Provider} from './containers';
 import {Navbar} from './components';
 import './App.css';
 // import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
