bbf796569d08b0790bc4fe59aa4a53a428a9cc0a
Tosin Aremu <66263731+Tohsyne@users.noreply.github.com>
2022-05-06 21:26:30 +0100

    blog section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import {Header, Footer, Blog, Download, Provider, Service, Testimonial, } from './containers';
-import {Header, Service, Provider, Download, Testimonial} from './containers';
+import {Header, Service, Provider, Download, Testimonial, Blog} from './containers';
 import {Navbar} from './components';
 import './App.css';
 // import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -15,6 +15,7 @@ const App = () => {
         <Provider />
         <Download />
         <Testimonial />
+        <Blog />
         {/* <Header />
         <button className='button'>
             Click Me
--- src/containers/index.js
+++ src/containers/index.js
@@ -6,3 +6,4 @@ export {default as Service} from './service/Service';
 export {default as Provider} from './provider/Provider';
 export {default as Download} from './download/Download';
 export {default as Testimonial} from './testimonial/Testimonial';
+export {default as Blog} from './blog/Blog';
